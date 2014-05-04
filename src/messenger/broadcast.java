d8c1cbf98d792c282be8006fece30971de0dd20e
Shung-Hsi Yu <shunghsiyu@gmail.com>
2014-05-04 00:09:48 -0400

    Updated broadcast.java Added Runnable as interface. Surround methods inside run() with try/catch.

--- src/messenger/broadcast.java
+++ src/messenger/broadcast.java
@@ -1,28 +1,34 @@
 package messenger;
+
 import java.net.*;
 import java.io.*;
-public class broadcast {
+
+
+public class broadcast implements Runnable {
 	private static String ip;
-    static int RECIEVE_LENGTH=1024;
-    static int BROADCAST_PORT = 9898;
-    static String BROADCAST_IP = "224.0.0.1";
-    InetAddress inetAddress = null;
+	static int RECIEVE_LENGTH = 1024;
+	static int BROADCAST_PORT = 9898;
+	static String BROADCAST_IP = "224.0.0.1";
+	InetAddress inetAddress = null;
 
-    public static void main(String args[]) throws IOException, Exception{
-        new broadcast().run();
-    }
-    @Override
-    public void run()throws Exception{
-        InetAddress receiveAddress=InetAddress.getByName(BROADCAST_IP);
-        int port=BROADCAST_PORT;
-        MulticastSocket receiveMulticast=new MulticastSocket(port);
-        receiveMulticast.joinGroup(receiveAddress);
-        DatagramPacket clientIP=new DatagramPacket(new byte[RECIEVE_LENGTH],RECIEVE_LENGTH);
-        receiveMulticast.receive(clientIP);
-        receiveMulticast.close();
-      
-    
-    }
+	@Override
+	public void run() {
+		try {
+			InetAddress receiveAddress = InetAddress.getByName(BROADCAST_IP);
+			int port = BROADCAST_PORT;
+			MulticastSocket receiveMulticast = new MulticastSocket(port);
+			receiveMulticast.joinGroup(receiveAddress);
+			DatagramPacket clientIP = new DatagramPacket(
+					new byte[RECIEVE_LENGTH], RECIEVE_LENGTH);
+			receiveMulticast.receive(clientIP);
+			receiveMulticast.close();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-   
 }
